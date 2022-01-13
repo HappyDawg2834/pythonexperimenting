6b32baa024ee4e166e813686e8ccbd933da3ceb1
HappyDawg2834 <87155699+HappyDawg2834@users.noreply.github.com>
2022-01-12 20:06:37 -0500

    got things working

--- hello.py
+++ hello.py
@@ -1,11 +1,13 @@
 import time
 
 
-print("Hello world")
+print("\nHello world")
 print("hi")
 x = input("What food do you like: ")
 print("You like "+x+"!")
 y = 8/2
 print(y)
+print(y)
+print("hi")
 print()
 time.sleep(2)
